1ddf0e72d850f20a5255384802120b0659fcf7b8
ItuTheGoat <itumelengmochabe@gmail.com>
2020-01-06 20:25:43 +0200

    Deleted that other thing and finished episode 8.

--- app.js
+++ app.js
@@ -1,2 +1,41 @@
 // Episode 8 - Node Event Emitter
-var stuff = require("./stuff");
+var events = require("events");
+
+// var util = require('util');
+/* looks good, doesn't work */
+
+class Person extends events.EventEmitter {
+  constructor(name) {
+    super();
+    this.name = name;
+  }
+}
+
+var James = new Person("James");
+var Jono = new Person("Jono");
+var King = new Person("King");
+
+var people = [James, Jono, King];
+
+people.forEach(function(person) {
+  person.on("speak", function(msg) {
+    console.log("Big man " + person.name + " said: " + msg + ", bruv");
+  });
+});
+
+// In order for the event to be called, It needs to be emitted
+James.emit("speak", "What shoes are those");
+King.emit("speak", "Deez are balenciagas");
+
+// util.inherits is not recommeded in the node versions.
+// util.inherits(Person, events.EventEmitter());
+
+// Use event emitter to react to certain events
+// var myEmitter = new events.EventEmitter();
+
+// listening for 'someEvent' which is a manually created event
+// myEmitter.on("someEvent", function(msg) {
+//   console.log(msg);
+// });
+
+// myEmitter.emit("someEvent", "the event was emitted");
