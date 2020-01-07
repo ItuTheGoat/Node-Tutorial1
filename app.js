b9a0b2ba38ec8393614cce69f9b357a10c63d46a
Itumeleng Mochabe <ItumelengMochabe@gmail.COM>
2020-01-07 15:21:10 +0200

    Created a pipe, now I will use it for the server

--- app.js
+++ app.js
@@ -6,12 +6,22 @@ var fs = require("fs");
 
 // Creates a readable stream for us
 var myReadStream = fs.createReadStream(__dirname + "/readMe.txt", "utf8");
+var myWriteStream = fs.createWriteStream(__dirname + "/writeMe.txt");
+
+// Taking what we're reading, and writing it into a text file.
+// myReadStream.on("data", function(chunk) {
+// 	console.log("new chunk received");
+// 	myWriteStream.write(chunk);
+// });
+
+// Easier way of doing the part above
+myReadStream.pipe(myWriteStream);
 
 // from the create read stream, we listen for the data event and receive data in chunks
-myReadStream.on("data", function(chunk) {
-	console.log("new chunk received");
-	console.log(chunk);
-});
+// myReadStream.on("data", function(chunk) {
+// 	console.log("new chunk received");
+// 	console.log(chunk);
+// });
 
 // // Request is what the client sends to the server
 // // Response is what the server communicates back
---
+++ writeMe.txt
@@ -0,0 +1,7 @@
+Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+
+Debitis voluptates ex saepe accusamus deserunt excepturi, 
+
+voluptatibus ad sed nesciunt ducimus aliquam distinctio similique dignissimos necessitatibus? 
+
+Voluptatem eaque inventore ex exercitationem?
