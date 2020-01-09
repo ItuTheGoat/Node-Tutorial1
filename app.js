d0f717035ef7ca013a0826114f3fad5a851b40c1
Itumeleng Mochabe <ItumelengMochabe@gmail.COM>
2020-01-09 14:38:51 +0200

    Completed episode 17, Serving JSON

--- app.js
+++ app.js
@@ -8,14 +8,22 @@ var fs = require("fs");
 // Response is what the server communicates back
 var server = http.createServer(function(req, res) {
 	console.log("request was made: " + req.url);
-	res.writeHead(200, { "Content-Type": "text/html" });
-	var myReadStream = fs.createReadStream(__dirname + "/index.html", "utf8");
+	res.writeHead(200, { "Content-Type": "application/json" });
 
-	myReadStream.pipe(res);
+	// Object which will be served
+	var jsonObject = {
+		name: "Bruce",
+		surname: "Wayne",
+		aEgo: "Batman",
+		type: "Hero"
+	};
+
+	// need to pass a string or buffer in the end() method
+	res.end(JSON.stringify(jsonObject));
 });
 
 // Specifying the port
 server.listen(3000, "127.0.0.1");
 console.log("Listening to port 3000");
 
-// Serving HTML
+// Serving JSON
