9ce8674868624530cd36570f072b2b9019898aed
Itumeleng Mochabe <ItumelengMochabe@gmail.COM>
2020-01-24 12:36:54 +0200

    Finished episode 30 which was Handling POST requests

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 // First we need to require express
 var express = require("express");
+var bodyParser = require("body-parser");
 
 // Setup the express app
 var app = express();
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.set("view engine", "ejs");
 // Middleware to serve up static files
@@ -15,6 +18,12 @@ app.get("/", function(req, res) {
 app.get("/contact", function(req, res) {
 	console.log(req.query);
 	res.render("contact", { qs: req.query });
+});
+
+// when we get the post request from contact. this middleware will parse it and store it in the request
+app.post("/contact", urlencodedParser, function(req, res) {
+	console.log(req.body);
+	res.render("contact-success", { data: req.body });
 });
 
 // Render a view
